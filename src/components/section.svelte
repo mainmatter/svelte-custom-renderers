fdde9d01b5afd439486e8c9f478b005c0dd95e5e
Florian Pichler <fp@ylk.gd>
2025-05-27 14:36:57 +0200

    Initial setup

---
+++ src/components/section.svelte
@@ -0,0 +1,20 @@
+<script>
+	let { children, title } = $props();
+
+</script>
+
+<section>
+	{#if title}
+		<h2>{title}</h2>
+	{/if}
+
+	{@render children()}
+</section>
+
+<style>
+	section {
+		padding-block: calc(var(--s-2xl) / 2);
+		text-align: center;
+		grid-column: c3 / c6;
+	}
+</style>
