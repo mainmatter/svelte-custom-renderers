45b8765ee3c1923cbc47a1a7b8a07fe73a4bdd64
Florian Pichler <fp@ylk.gd>
2025-05-27 19:43:07 +0200

    Stub stage component

--- src/components/stage.svelte
+++ src/components/stage.svelte
@@ -1,30 +1,74 @@
 <script>
-	let code = 'Never gonna give you up, never gonna let you down\nNever gonna run around and desert you\nNever gonna make you cry, never gonna say goodbye\nNever gonna tell a lie and hurt you\nNever gonna give you up, never gonna let you down\nNever gonna run around and desert you\nNever gonna make you cry, never gonna say goodbye\nNever gonna tell a lie and hurt you'.split('').map((c) => c.charCodeAt(0).toString(2).padStart(8, '0')).join('').split('');
+	let data = 'Never gonna give you up, never gonna let you down\nNever gonna run around and desert you\nNever gonna make you cry, never gonna say goodbye\nNever gonna tell a lie and hurt you\nNever gonna give you up, never gonna let you down\nNever gonna run around and desert you\nNever gonna make you cry, never gonna say goodbye\nNever gonna tell a lie and hurt you'.split('').map((c) => c.charCodeAt(0).toString(2).padStart(8, '0')).join('').split('');
 
 	function rand() {
-		return Math.floor(Math.random() * 11);
+		return Math.floor(Math.random() * 12);
 	}
+
+	// Random bit flips
+	// function flip(element) {
+	// 	let on = (event) => {
+	// 		if (Math.random < 0.5) {
+	// 			return;
+	// 		}
+	// 		event.target.textContent = event.target.textContent === '1' ? '0' : '1';
+	// 	};
+
+	// 	element.addEventListener('animationiteration', on);
+
+	// 	return () => {
+	// 		element.removeEventListener('animationiteration', on);
+	// 	}
+	// }
 </script>
 
 <section>
-	<div class="code">
-		{#each code as c, i (i)}
+	<div class="data" role="presentation" aria-hidden="true">
+		{#each data as c, i (i)}
 			<span class={`c${rand()}`}>{c}</span>
 		{/each}
 	</div>
+
+	<code class="code"><pre>&lt;<span>script</span>&gt;
+// ...
+&lt;/<span>script</span>&gt;
+
+&lt;Catalog&gt;
+  &lt;List&gt;
+    &lt;Image click=&quot;navigate()&quot; /&gt;
+  &lt;/List&gt;
+&lt;/Catalog&gt;
+
+&lt;Toolbar /&gt;
+
+&lt;style&gt;
+  /* ... */
+&lt;/style&gt;</pre></code>
 </section>
 
 <style>
 	section {
-		grid-column: 1 / -1;
+		display: grid;
+		grid-template-columns: subgrid;
+		grid-column: leading / trailing;
+		overflow-x: hidden;
 	}
 
-	.code {
+	.data {
+		grid-column: c1 / c6;
+		grid-row: 1 / 3;
+		mask: radial-gradient(closest-side, black 30%, transparent 100% );
 		word-break: break-all;
 		font-family: var(--font-mono);
+		overflow: hidden;
+		height: 24lh;
+		-webkit-user-select: none;
+		user-select: none;
+		font-size: var(--fs-sm);
+		line-height: 1.1;
 	}
 
-	span {
+	.data span {
 		margin: -0.5em;
 		padding: 0.5em;
 		--step: 0.5s;
@@ -33,51 +77,51 @@
 		animation: blink calc(var(--step) * 12) infinite ease-in;
 	}
 
-	.c0 {
+	.data .c0 {
 		animation-delay: calc(var(--step) * -11);
 	}
 
-	.c1 {
-		animation-delay: calc(var(--step) * -10);
+	.data .c1 {
+		animation-delay: calc(var(--step) * -9.1);
 	}
 
-	.c2 {
-		animation-delay: calc(var(--step) * -9);
+	.data .c2 {
+		animation-delay: calc(var(--step) * -9.2);
 	}
 
-	.c3 {
-		animation-delay: calc(var(--step) * -8);
+	.data .c3 {
+		animation-delay: calc(var(--step) * -8.3);
 	}
 
-	.c4 {
-		animation-delay: calc(var(--step) * -7);
+	.data .c4 {
+		animation-delay: calc(var(--step) * -7.4);
 	}
 
-	.c5 {
-		animation-delay: calc(var(--step) * -6);
+	.data .c5 {
+		animation-delay: calc(var(--step) * -6.5);
 	}
 
-	.c6 {
-		animation-delay: calc(var(--step) * -5);
+	.data .c6 {
+		animation-delay: calc(var(--step) * -5.6);
 	}
 
-	.c7 {
-		animation-delay: calc(var(--step) * -4);
+	.data .c7 {
+		animation-delay: calc(var(--step) * -4.7);
 	}
 
-	.c8 {
-		animation-delay: calc(var(--step) * -3);
+	.data .c8 {
+		animation-delay: calc(var(--step) * -3.8);
 	}
 
-	.c9 {
-		animation-delay: calc(var(--step) * -2);
+	.data .c9 {
+		animation-delay: calc(var(--step) * -2.9);
 	}
 
-	.c10 {
+	.data .c10 {
 		animation-delay: calc(var(--step) * -1);
 	}
 
-	.c11 {
+	.data .c11 {
 		animation-delay: 0;
 	}
 
@@ -90,5 +134,26 @@
 			color: var(--fg-primary);
 			text-shadow: 0 0 0.5em var(--fg-primary);
 		}
+	}
+
+	@media (prefers-reduced-motion: reduce) {
+		.data span {
+			animation: none;
+		}
+	}
+
+	.code {
+		position: relative;
+		grid-column: leading / c2;
+		grid-row: 1 / span 1;
+		background: #23272F;
+		margin-left: var(--grid-gutter);
+		border: 1px solid #303541;
+		box-shadow: 0px 1px 2px rgba(0, 0, 0, 0.15), 0px 2px 10px rgba(0, 0, 0, 0.15), 0px 2px 40px rgba(0, 0, 0, 0.28);
+		border-radius: var(--s-xs);
+		font-size: 0.8125rem;
+		line-height: 1.3;
+		padding: var(--s-sm);
+		overflow: hidden;
 	}
 </style>
