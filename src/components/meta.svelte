0af8a89bd5db65c02eccf3a98e9dedb0967d4bb0
Marco Otte-Witte <marco.otte-witte@mainmatter.com>
2025-06-03 20:58:59 +0200

    make some test changes

--- src/components/meta.svelte
+++ src/components/meta.svelte
@@ -1,6 +1,4 @@
 <script>
-	import ogImage from '../assets/og-image.jpg';
-
 	let { title, description, url } = $props();
 </script>
 
@@ -10,12 +8,10 @@
 
 	<meta property="og:url" content="https://{url}" />
 	<meta property="og:type" content="website" />
-	<meta property="og:image" content={ogImage} />
 	<meta property="og:title" content={title} />
 	<meta property="og:description" content={description} />
 
 	<meta name="twitter:card" content="summary_large_image" />
-	<meta name="twitter:image" content={ogImage} />
 	<meta property="twitter:domain" content={url} />
 	<meta property="twitter:url" content="https://{url}" />
 	<meta name="twitter:title" content={title} />
--- src/components/section.svelte
+++ src/components/section.svelte
@@ -13,15 +13,4 @@
 </section>
 
 <style>
-	section {
-		display: grid;
-		grid-template-columns: subgrid;
-		grid-column: leading / trailing;
-	}
-
-	div {
-		padding-block: var(--s-xl);
-		text-align: center;
-		grid-column: c2 / c5;
-	}
 </style>
